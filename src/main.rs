68161c8821c12da0cdf86b5479b25bacca82dd8a
salinas <80262440+eoSalinas@users.noreply.github.com>
2026-03-15 18:47:35 -0300

    feat: flatten multiple locale files in a dir

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{collections::HashSet, fs::read_to_string};
+use walkdir::WalkDir;
 
 use serde_json::Value;
 
@@ -29,16 +30,24 @@ fn flatten_into(value: &Value, buf: &mut String, out: &mut HashSet<String>) {
 }
 
 fn main() {
-    // TODO: handle multiple locale files
+    // TODO: based on user input or config file
+    let locales_dir = "./fixtures/locales";
+
     // TODO: evaluate and handle unwraps
+    for entry in WalkDir::new(locales_dir) {
+        let entry = entry.unwrap();
+        // TODO: add better check for json
+        if entry.file_type().is_file() {
+            let mut buf = String::new();
+            let mut out = HashSet::new();
 
-    let content = read_to_string("./fixtures/locales/Auth/Login.json").unwrap();
-    let deserialized: Value = serde_json::from_str(&content).unwrap();
+            let content = read_to_string(entry.path()).unwrap();
+            let deserialized: Value = serde_json::from_str(&content).unwrap();
+            flatten_into(&deserialized, &mut buf, &mut out);
 
-    let mut buf = String::new();
-    let mut out = HashSet::new();
-
-    flatten_into(&deserialized, &mut buf, &mut out);
-
-    println!("{:?}", out);
+            println!("File: {}", entry.path().display());
+            println!("{:?}", out);
+            println!();
+        }
+    }
 }
